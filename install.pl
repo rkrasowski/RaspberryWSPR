50119d77094b4c7db544f533a5265d389c7aec32
Robert J Krasowski <rkrasowski@yahoo.com>
2015-03-17 00:12:35 +0000

    Added update

--- install.pl
+++ install.pl
@@ -2,6 +2,22 @@
 use strict;
 use warnings;
 
-print "Starting script, please be patient, it will take 20 min to install WSPR software !!!\n\n";
+print "\n\nStarting script, please be patient, it will take 20 min to install WSPR software !!!\n\n";
+print "Update system\n";
+update();
+print "Add jessie to sources.list\n";
 
 
+
+
+########################################################################################
+sub update
+	{
+		open(my $UPDATE,"apt-get update |") || die "Failed: $!\n";
+		while ( <$UPDATE> )
+			{
+  				print $_;
+			}
+ 
+		close($UPDATE);
+	}
